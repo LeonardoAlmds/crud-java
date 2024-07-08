ee04e6d1e7071c2c9f8ed180a2e26f70614c32eb
LeonardoAlmds <leonardoalmd8@outlook.com>
2024-07-07 23:58:18 -0300

    fixing java example

--- src/com/leonardoalmd/App.java
+++ src/com/leonardoalmd/App.java
@@ -1,7 +1,6 @@
 package com.leonardoalmd;
 
 import com.leonardoalmd.DAO.ContactDAO;
-import com.leonardoalmd.factory.ConnectionFactory;
 import com.leonardoalmd.model.Contact;
 
 public class App {
--- src/com/leonardoalmd/factory/ConnectionFactoryExample.java
+++ src/com/leonardoalmd/factory/ConnectionFactoryExample.java
@@ -4,14 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionFactory {
+public class ConnectionFactoryExample {
     private static final String url = "jdbc:mysql://localhost:3306/";
     private static final String user = "root";
     private static final String password = "";
 
     private static Connection conn;
 
-    public static Connection createConnectionToMySQL() {
+    public static Connection createConnectionToMySQLExample() {
         try {
             if (conn == null) {
                 conn = DriverManager.getConnection(url, user, password);
